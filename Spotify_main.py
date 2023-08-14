199200924aed3ffa84cc729e20acce7d38956955
Aldoil <piotrkowalski1297@gmail.com>
2023-08-14 18:52:19 +0200

    Version 1

--- Spotify_main.py
+++ Spotify_main.py
@@ -2,12 +2,45 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import plotly.express as px 
+import plotly.graph_objects as go
 
 
 
-#Read data
-df = pd.read_csv('Clean_data.csv')
+def get_data():
+    json_data_list = []
+    # Allow multiple file uploads
+    uploaded_files = st.file_uploader("Choose JSON files", accept_multiple_files=True)
+    for file in uploaded_files:
+        # Read the uploaded JSON file
+        data = pd.read_json(file)
+        json_data_list.append(data)
 
+    try:    
+        df = pd.concat(json_data_list, ignore_index =True)
+        return df
+    except:
+        st.title('There is no data')
+        
+def data_processing(df):
+    df = df.drop(columns=['username', 'user_agent_decrypted', 'spotify_track_uri', 'spotify_episode_uri', 
+                      'reason_start', 'reason_end', 'offline_timestamp', 'incognito_mode', 'ip_addr_decrypted'])
+    pattern_1 = r'([^ ]+)'
+    pattern_2 = r'\((.*?)\)'
+
+    # Use str.extract to split the 'platform' column
+    df['System'] = df['platform'].str.extract(pattern_1)[0].str.upper()
+    df['Device'] = df['platform'].str.extract(pattern_2)[0].str.upper()
+    df['System'] = df['System'].replace('OSX', 'OS')
+
+    #Deal with dates 
+    df['ts'] = pd.to_datetime(df['ts'])
+    df['date'] = df['ts'].dt.strftime('%Y-%m-%d')
+    df['time'] = df['ts'].dt.strftime('%H:%M')
+    df = df.drop(['ts', 'platform'], axis=1)
+    # Create type column
+    df['Type'] = np.where(df['episode_name'].notna() , 'Podcast', 'Song')
+
+    return df
 
 def visualize(df):
 
@@ -32,6 +65,7 @@ def visualize(df):
     audio_type = st.sidebar.multiselect('Audio type', df['Type'].unique().tolist())
     artist = st.sidebar.multiselect('Artist', ['All'] + df['master_metadata_album_artist_name'].unique().tolist())
     song = st.sidebar.multiselect('Song', ['All'] + df['master_metadata_track_name'].unique().tolist())
+    country = st.sidebar.multiselect('Listening country', ['All'] + df['conn_country'].unique().tolist())
 
     df = df[(df['date'] >= first_date) & (df['date'] <= last_date)]
 
@@ -45,6 +79,8 @@ def visualize(df):
         df = df[df['master_metadata_album_artist_name'].isin(artist)]
     if song and song != ['All']:
         df = df[df['master_metadata_track_name'].isin(song)]
+    if country and country != ['All']:
+        df = df[df['conn_country'].isin(country)]
 
 
     ### Information part ###
@@ -52,7 +88,10 @@ def visualize(df):
                 which is {round(df['ms_played'].sum()/3600000, 2)} hours \
                 which is {round(df['ms_played'].sum()/3600000/24, 2)} days.")
     st.info(f"Number of unique artists listened: {df['master_metadata_album_artist_name'].nunique()}")
-    st.info(f"Number of unique songs/podcasts listened: {df['master_metadata_track_name'].nunique()}")
+    st.info(f"Number of unique songs listened: {df['master_metadata_track_name'].nunique()}")
+    st.info(f"Number of unique podcasts listened: {df['episode_show_name'].nunique()}")
+    st.info(f"Number of unique podcasts episodes listened: {df['episode_name'].nunique()}")
+
 
     
     ### Date plots ###
@@ -79,7 +118,20 @@ def visualize(df):
     line_date_plot.update_xaxes(range=[df_min_played['date'].min(), df_min_played['date'].max()] )
 
     st.plotly_chart(line_date_plot)
-    df_min_played
+
+
+    ### Time plot ###   
+    df['time'] = pd.to_datetime(df['time'], format='%H:%M') #Prepare data
+    df['time'] = df['time'].dt.hour
+
+    df_time_grouped = df.groupby(df['time'])['ms_played'].sum().reset_index()
+    df_time_grouped['Minutes played'] = round(df_time_grouped['ms_played']/60000, 2)
+    df_time_grouped['Hours played'] = round(df_time_grouped['ms_played']/3600000, 2)
+    df_time_grouped['Days played'] = round(df_time_grouped['Hours played'] / 24, 2)                             
+    
+    time_fig = px.bar(data_frame=df_time_grouped, x='time', y='Minutes played', title='Minutes played per hour of the day', hover_data=['Hours played', 'Days played'])
+    st.plotly_chart(time_fig)
+
 
     ### Artists plot ###
 
@@ -90,23 +142,26 @@ def visualize(df):
     df_artist_gruped.rename(columns={'master_metadata_album_artist_name': 'Artist'}, inplace=True)
     df_artist_gruped = df_artist_gruped.sort_values('Minutes played', ascending=False).head(20)
 
-    artist_plot = px.pie(df_artist_gruped, values='Hours played', names='Artist', title='Most listened artists')
+    artist_plot = px.pie(df_artist_gruped, values='Hours played', names='Artist', title='Most listened artists (Top 20)')
     st.plotly_chart(artist_plot)
 
-
     ### Artists plot ###
-    df_songs_gruped = df.groupby('master_metadata_track_name')['ms_played'].sum().reset_index()
+    df_songs_gruped = df.groupby(['master_metadata_album_artist_name', 'master_metadata_track_name'])['ms_played'].sum().reset_index()
     df_songs_gruped['Minutes played'] = round(df_songs_gruped['ms_played']/60000, 2)
     df_songs_gruped['Hours played'] = round(df_songs_gruped['ms_played']/3600000, 2)
     df_songs_gruped['Days played'] = round(df_songs_gruped['Hours played'] / 24, 2)
-    df_songs_gruped.rename(columns={'master_metadata_track_name': 'Song'}, inplace=True)
-    df_songs_gruped = df_songs_gruped.sort_values('Minutes played', ascending=True).head(20)
+    df_songs_gruped.rename(columns={'master_metadata_track_name': 'Song', 'master_metadata_album_artist_name': 'Artist'}, inplace=True)
+    df_songs_gruped = df_songs_gruped.sort_values('Minutes played', ascending=False).head(20)
 
-    songs_plot = px.pie(df_songs_gruped, values='Minutes played', names='Song', title='Most played songs')
+    df_songs_gruped['Artist '] = df_songs_gruped['Artist'] + ' | Hours played: ' + df_songs_gruped['Hours played'].astype(str) #Create hover
+
+    # Plot using 'hover_info' as hover data
+    songs_plot = px.pie(df_songs_gruped, values='Minutes played', names='Song', title='Most played songs (Top 20)', hover_data=['Artist '])
     st.plotly_chart(songs_plot)
 
 
 
-
+df = get_data()
+df = data_processing(df)
 visualize(df)
 
